4f215349f3aafe7996936e4508b5446ff3c565a4
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-06-30 00:15:32 +0500

    Basic Wordpress Theme Created.

---
+++ theme/footer.php
@@ -0,0 +1,8 @@
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="assets/js/jQuery.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="assets/js/popper.js"></script>
+<script src="assets/js/bootstrap.js"></script>
+</body>
+
+</html>
