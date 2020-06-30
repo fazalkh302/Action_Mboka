547c2aabc3bf386cda787dc8fcdcd031384253b6
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-06-30 18:47:03 +0500

    theme added.

---
+++ theme/header.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- bootstrap css -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Awosome fonts icons -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/solid.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/regular.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/brands.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/fontawesome.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        @font-face {
+            font-family: my-font;
+            src: url(assets/fonts/Basic-Regular.ttf);
+        }
+    </style>
+</head>
+
+<body>
+
+    <!-- top contact area -->
+    <div class="container-fluid borders border-dark py-1 text-white position-relative"
+        style="box-shadow: 0px 5px 12px 3px #0000006b; background-color: #1d2a3b;">
+        <div class="container borders border-warning">
+            <!-- <div class="col-lg-12 borders border-danger"> -->
+            <div class="row align-items-centers">
+                <div class="col-lg-2 borders border-primary">
+                    <h6 class="m-0 mt-1" style="font-size: small;">admin@admin.com</h6>
+                </div>
+                <div class="col-lg-3 borders border-primary">
+                    <h6 class="m-0 mt-1" style="font-size: small;">+9200000000</h6>
+                </div>
+                <div class="col-lg-3 ml-auto pr-0s border-primary borders">
+                    <div class="col-12">
+                        <div class="row">
+                            <i class="fab fa-facebook-f px-2 mt-2s" style="font-size: small; line-height: 2;"></i>
+                            <i class="fab fa-twitter px-2" style="font-size: small; line-height: 2;"></i>
+                            <i class="fab fa-instagram px-2 ml-autos" style="font-size: small; line-height: 2;"></i>
+                            <a href="#" class="ml-auto text-white">
+                                <h6 class="h6 mt-1" style="font-size: small;">Login</h6>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- </div> -->
+        </div>
+    </div>
+
+    <!-- navbar in container -->
+    <div class="container-fluid borders border-dark bg-dangers" style="background-color: #03b664;">
+        <div class="container borders border-warning">
+            <!-- start navbar -->
+            <nav class="navbar navbar-expand-lg navbar-light borders border-dark px-0">
+                <a class="navbar-brand borders border-primary col-2" href="#">
+                    <img src="assets/images/Action_20MBOKA-04 (1).png" class="img-fluid" alt="">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse borders border-warning" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto m-auto borders border-primary">
+                        <li class="nav-item active">
+                            <a class="nav-link mx-2 text-white"
+                                style="font-weight: 600; border-bottom: 2px solid #4362af;" href="#">Home <span
+                                    class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link mx-2 text-white" style="font-weight: 600;" href="#">Mission</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link mx-2 text-white" style="font-weight: 600;" href="#">Objective</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link mx-2 text-white" style="font-weight: 600;" href="#">Make Donation</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link mx-2 text-white" style="font-weight: 600;" href="#">News</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link mx-2 text-white" style="font-weight: 600;" href="#">About Us</a>
+                        </li>
+                    </ul>
+                    <form class="form-inline">
+                        <button class="btn btn-warning py-2 px-4 rounded-sm" style="font-size: small; font-weight: 600;"
+                            type="submit">DONATE NOW</button>
+                    </form>
+                </div>
+            </nav>
+            <!-- end navbar -->
+        </div>
+    </div>
---
+++ theme/index.php
@@ -0,0 +1,43 @@
+<?php get_header();?>
+
+<!-- MainPage Section -->
+<div class="container-fluid border border-dark heros bg">
+        <div class="container position-absolutes borders border-warning" style="margin-top: 7rem;">
+            <div class="row">
+                <div class="col-6 ml-auto border-primary borders my-auto">
+                    <h1 class="display-2 text-white" style="color: white !important; font-family: my-font;">Habitate for
+                        Humanity Donations</h1>
+                </div>
+                <div class="col-5 ml-auto border-danger py-5 px-5 borders bg-white rounded-lg">
+                    <form action="">
+                        <h1 class="mb-5 text-monospaces" style="font-family: monospace;">Become a volunteer to help
+                            helpless.</h1>
+                        <div class="form-group">
+                            <input type="text" class="form-control shadow-sm rounded-lg py-4" style="border-color: #ced4da00;     font-size: revert;
+    font-weight: 600;" placeholder="Full Name" name="" id="">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control shadow-sm py-4" style="border-color: #ced4da00;     font-size: revert;
+    font-weight: 600;" placeholder="Email Address" name="" id="">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control shadow-sm py-4" style="border-color: #ced4da00;     font-size: revert;
+    font-weight: 600;" placeholder="Why You Join With Us" name="" id="">
+                        </div>
+                        <div class="form-group">
+                            <textarea name="" class="form-control shadow-sm py-4 h-50" placeholder="Write Message"
+                                style="border-color: #ced4da00;     font-size: revert;
+    font-weight: 600;" id="" cols="30" rows="6"></textarea>
+                        </div>
+                        <div class="form-group m-0">
+                            <button type="button" class="btn btn-success col-6 rounded shadow py-2"
+                                style="font-weight: 500;">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+<!-- ./End MainPage -->
+
+<?php get_footer();?>
