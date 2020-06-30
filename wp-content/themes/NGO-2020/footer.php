ee17842a0b83f2290d145743bfa3601bb5ec2291
fazalkh302 <62914421+fazalkh302@users.noreply.github.com>
2020-06-30 23:10:37 +0500

    Custom wordpress theme UPDATE

---
+++ wp-content/themes/NGO-2020/footer.php
@@ -0,0 +1,83 @@
+<!-- become a donar -->
+<div class="col-lg-12 borders" style="padding-right: 76px; padding-left: 77px;">
+        <div class="container borders p-5 text-white" style="background-color: #03b664; top: 100px; position: relative;">
+            <div class="col-lg-12 borders p-4">
+                <div class="col-lg-8 borders text-center m-auto">
+                    <h1 class="myfont1 text-center">Become a Donar</h1>
+                    <p class="text-center mb-5" style="font-size: small;">Even students who are just learning to write,school and middle school, they can begin to create paragraphs that include all of the necessary they should be able to write a paragraph</p>
+                    <button type="button" class="btn btn-warning col-6 rounded shadow-sm py-2" style="font-weight: 500;">DONATE</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- footer -->
+    <footer class="bg-light" style="padding-top: 10rem; padding-bottom: 3rem;">
+        <div class="container borders">
+            <div class="col-lg-12 borders">
+                <div class="row">
+                    <div class="col-lg-4 px-0 borders">
+                        <div class="col-4 borders px-0 mb-4">
+                            <img src="<?php bloginfo('template_url'); ?>/assets/images/Action_20MBOKA-04 (1).png" class="borders card-img" alt="">
+                        </div>
+                        <div class="col-lg-12 px-0">
+                            <p class="text-muteds myfont1" style="font-size: small;">Phone (Pk) : +92 000 000 000</p>
+                            <p class="text-muteds myfont1 mb-4" style="font-size: small;">SD-1 Block 'A' North Nazimabad, Karachi - 75760, Pakistan</p>
+                            <i class="fab fa-facebook-f bg-white shadow-sm mx-1 rounded-circle"></i>
+                            <i class="fab fa-twitter bg-white shadow-sm mx-1 rounded-circle"></i>
+                            <i class="fab fa-instagram bg-white shadow-sm mx-1 rounded-circle"></i>
+                            <i class="fab fa-snapchat-ghost bg-white shadow-sm mx-1 rounded-circle"></i>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 borders">
+                        <div class="col-lg-12 borders mb-4">
+                            <h3 class="myfont1">Service</h3>
+                        </div>
+                        <div class="col-lg-12 borders">
+                            <p class="myfont1" style="font-size: small;">Donate</p>
+                            <p class="myfont1" style="font-size: small;">Volunteer</p>
+                            <p class="myfont1" style="font-size: small;">Donation</p>
+                            <p class="myfont1" style="font-size: small;">Chairty</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 borders">
+                        <div class="col-lg-12 borders mb-4">
+                            <h3 class="myfont1">Our Links</h3>
+                        </div>
+                        <div class="col-lg-12 borders">
+                            <p class="myfont1" style="font-size: small;"><a class="text-dark" href="#">Contact Us</a></p>
+                            <p class="myfont1" style="font-size: small;"><a class="text-dark" href="#">About Us</a></p>
+                            <p class="myfont1" style="font-size: small;"><a class="text-dark" href="#">Donation</a></p>
+                            <p class="myfont1" style="font-size: small;"><a class="text-dark" href="#">Volunteer</a></p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="col-lg-12 mb-4">
+                            <h3 class="myfont1 mb-1">Subscribe</h3>
+                            <p class="myfont1" style="font-size: small;">Even students who are just</p>
+                        </div>
+                        <div class="col-lg-12">
+                            <form action="" method="post" class="">
+                                <input type="email" name="" id="" class="form-control col mb-2 rounded-lg py-4 border-0 shadow-sm" style="font-size: revert;font-weight: 600;" placeholder="Enter your Email for Donate" required>
+                                <button type="button" class="btn btn-warning col-5 myfont1 py-2 px-4">Send</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <div class="container-fluid p-3 text-center" style="background-color:#03b664 !important; color: white !important;">
+        <h6 class="m-0 font-small">&copyCopyright <?php echo date('Y'); ?> <?php bloginfo('name'); ?> - Design By: <a href="#" class="text-warning">@FAZAL</a></h6>
+    </div>
+
+    <?php wp_footer(); ?>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+	<script src="assets/js/jQuery.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="assets/js/popper.js"></script> 
+	<script src="assets/js/bootstrap.js"></script>
+</body>
+</html>
---
+++ wp-content/themes/NGO-2020/index.php
@@ -0,0 +1,308 @@
+<?php get_header();?>
+
+
+<!-- start Main Page Section -->
+
+<!-- Hero-image with Inner-Content -->
+<div class="container-fluid border border-dark heros bg" style="margin-bottom: 7rem;">
+        <div class="container position-absolutes borders border-warning" style="margin-top: 7rem;">
+            <div class="row">
+                <div class="col-6 ml-auto border-primary borders my-auto">
+                    <h1 class="display-2 text-white" style="color: white !important; font-family: myfont1;">Habitate for Humanity Donations</h1>
+                </div>
+                <div class="col-5 ml-auto border-danger py-5 px-5 borders bg-white rounded-lg">
+                    <form action="">
+                        <h1 class="mb-5 text-monospaces" style="font-family: monospace;">Become a volunteer to help helpless.</h1>
+                        <div class="form-group">
+                            <input type="text" class="form-control shadow-sm rounded-lg py-4" style="border-color: #ced4da00;     font-size: revert;
+    font-weight: 600;" placeholder="Full Name" name="" id="">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control shadow-sm rounded-lg py-4" style="border-color: #ced4da00;     font-size: revert;
+    font-weight: 600;" placeholder="Email Address" name="" id="">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control shadow-sm rounded-lg py-4" style="border-color: #ced4da00;     font-size: revert;
+    font-weight: 600;" placeholder="Why You Join With Us" name="" id="">
+                        </div>
+                        <div class="form-group">
+                            <textarea name="" class="form-control shadow-sm rounded-lg py-4 h-50" placeholder="Write Message" style="border-color: #ced4da00;     font-size: revert;
+    font-weight: 600;" id="" cols="30" rows="6"></textarea>
+                        </div>
+                        <div class="form-group m-0">
+                            <button type="button" class="btn btn-warning col-6 rounded shadow-sm py-2" style="font-weight: 500;">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END-Hero-image with Inner-Content -->
+
+    <!-- for-good-recent-causes-heading -->
+    <div class="container borders border-danger">
+        <div class="col-lg-12">
+            <div class="row justify-content-center">
+                <div class="col-lg-6 border-danger borders">
+                    <h2 class="h2 m-0s mb-3 text-center myfont1">For Good Recent Causes</h2>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-lg-6 borders border-primary">
+                    <h6 class="text-center font-small myfont1 text-muted">To use the font for an HTML element, refer to the name of the font (myFirstFont) through the font-family property:</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END-for-good-recent-causes-heading -->
+
+    <!-- START for-good-recent-causes-CARDs -->
+    <div class="container mt-5 css" style="margin-bottom: 7rem;">
+        <div class="card-deck">
+            <!-- START 1st-card -->
+            <div class="card">
+                <img src="<?php bloginfo('template_url'); ?>/assets/images/c1.jpg" class="card-img-top shadow-sm" alt="...">
+                <div class="card-body rounded-lg shadow">
+                    <h5 class="card-title myfont1">Card title</h5>
+                    <p class="card-text font-small text-muted mb-4">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="progress mt-4 mb-2">
+                        <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+                    </div>
+                    <div class="col">
+                        <div class="row">
+                            <h6 class="font-small mr-auto">Raised: $20,99</h6>
+                            <h6 class="font-small"> Goal: $15,99</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- START 2nd-card -->
+            <div class="card">
+                <img src="<?php bloginfo('template_url'); ?>/assets/images/c1.jpg" class="card-img-top shadow-sm" alt="...">
+                <div class="card-body rounded-lg shadow">
+                    <h5 class="card-title myfont1">Card title</h5>
+                    <p class="card-text font-small text-muted mb-4">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="progress mt-4 mb-2">
+                        <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+                    </div>
+                    <div class="col">
+                        <div class="row">
+                            <h6 class="font-small mr-auto">Raised: $20,99</h6>
+                            <h6 class="font-small"> Goal: $15,99</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- START 3rd-card -->
+            <div class="card">
+                <img src="<?php bloginfo('template_url'); ?>/assets/images/c1.jpg" class="card-img-top shadow-sm" alt="...">
+                <div class="card-body rounded-lg shadow">
+                    <h5 class="card-title myfont1">Card title</h5>
+                    <p class="card-text font-small text-muted mb-4">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="progress mt-4 mb-2">
+                        <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+                    </div>
+                    <div class="col">
+                        <div class="row">
+                            <h6 class="font-small mr-auto">Raised: $20,99</h6>
+                            <h6 class="font-small"> Goal: $15,99</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- END 3rd-card -->
+        </div>
+    </div>
+    <!-- END-for-good-recent-causes-CARDs -->
+
+    <!-- START - MAIN-Container of read-more-section-with-small-cards -->
+    <div class="container border-primary borders px-0" style="margin-top: 6rem; margin-bottom: 7rem;">
+        <div class="col-lg-12 border-primary borders">
+            <div class="row">
+                <!-- START read-more-section -->
+                <div class="col-lg-5 borders mr-auto border-danger">
+                    <h1 class="h2 myfont1 font mb-3">We help thousands of children to get their Education.</h1>
+                    <h6 class="m-0 text-muted font-small">Even students who are just learning to write,school and middle school, they can begin to create paragraphs that include all of the necessary elements. Once a student reaches high school, they should be able to write a paragraph that has a distinct thesis and also includes supporting arguments.</h6>
+                    <br>
+                    <h6 class="text-muted font-small mb-4">Even students who are just learning to write. Once a student reaches high school, they should be able to write a paragraph that has a distinct thesis and also includes supporting arguments.</h6>
+                    <button type="button" class="btn btn-warning myfont1 py-2 px-4 rounded-sm">Read More</button>
+                </div>
+                <!-- END read-more-section -->
+
+                <!-- START small-cards -->
+                <div class="col-lg-6 border-warning borders">
+                    <div class="card mb-3 shadow-sms border-0" style="max-width: 540px;">
+                        <div class="row no-gutters">
+                          <div class="col-md-3">
+                            <img src="<?php bloginfo('template_url'); ?>/assets/images/c1.jpg" class="card-img" alt="...">
+                          </div>
+                          <div class="col-md-8">
+                            <div class="card-body py-0">
+                                <h5 class="card-title myfont1 mb-2">Make Donation</h5>
+                                <p class="card-text font-small text-muted">This is a wider card with to additional content. This content is a little bit longer.</p>
+                              </div>
+                          </div>
+                        </div>
+                    </div>
+                    <div class="card mb-3 shadow-sms border-0" style="max-width: 540px;">
+                        <div class="row no-gutters">
+                          <div class="col-md-3">
+                            <img src="<?php bloginfo('template_url'); ?>/assets/images/c1.jpg" class="card-img" alt="...">
+                          </div>
+                          <div class="col-md-8">
+                            <div class="card-body py-0">
+                                <h5 class="card-title myfont1 mb-2">Make Donation</h5>
+                                <p class="card-text font-small text-muted">This is a wider card with to additional content. This content is a little bit longer.</p>
+                              </div>
+                          </div>
+                        </div>
+                    </div>
+                    <div class="card mb-3 shadow-sms border-0" style="max-width: 540px;">
+                        <div class="row no-gutters">
+                          <div class="col-md-3">
+                            <img src="<?php bloginfo('template_url'); ?>/assets/images/c1.jpg" class="card-img" alt="...">
+                          </div>
+                          <div class="col-md-8">
+                            <div class="card-body py-0">
+                                <h5 class="card-title myfont1 mb-2">Make Donation</h5>
+                                <p class="card-text font-small text-muted">This is a wider card with to additional content. This content is a little bit longer.</p>
+                              </div>
+                          </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- END small-cards -->
+            </div>
+        </div>
+    </div>
+    <!-- END - MAIN-Container of read-more-section-with-small-cards -->
+
+    <!-- START who-we-are -->
+    <div class="container-fluid p-0 text-white" style="margin-bottom: 7rem;">
+        <div class="card rounded-0 border-0">
+            <picture>
+                <img class="w-100" src="<?php bloginfo('template_url'); ?>/assets/images/charity-donation-donor-collect.png">
+            </picture>
+            <div class="card-body position-absolute" style="top: 100px;">
+                <div class="col-lg-12 pl-5">
+                    <div class="col-lg-4 mb-3">
+                        <h2 class="myfont1">WHO WE ARE</h2>
+                    </div>
+                    <div class="col-lg-6 mb-5">
+                        <p class="myfont1" style="font-size: small;">Even students who are just learning to  middle school write,school and middle school, school and middle school they can begin to create paragraphs that include all of the necessary elements. Once a student reaches high school, they should be able to write a paragraph that has a distinct thesis and also includes supporting arguments.</p>
+                        <p class="myfont1" style="font-size: small;">Paragraphs that include all of the necessary all of the necessary elements. Once a student reaches high school, they should be able to write a paragraph that has a distinct thesis and also includes supporting arguments.</p>
+                    </div>
+                    <div class="col-lg-3">
+                        <form action="#" method="post">
+                            <button type="button" class="btn btn-warning w-75 myfont1 py-2 px-4">Who We Are</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END who-we-are -->
+
+    <!-- START - Main-Container of what-we-doo -->
+    <div class="container mb-5 borders border-primary px-0">
+        <div class="col-lg-12 text-center borders border-warning mb-5">
+            <h2 class="myfont1">What We Do</h2>
+            <h6 class="font-small">This is a wider card with to additional content. This content is <br> This content is a little bit</h6>
+        </div>
+        <div class="col-lg-12 border-danger borders sd-flex">
+            <div class="row justify-content-betweens sno-gutters">
+                <!-- START 1st-card -->
+                <div class="col-lg-4 sw-25 sborder bg-lights p-3s spy-3 mx-1s sshadow">
+                    <div class="col-lg-12 border-primary shadow-sm rounded-lg py-4 bg-light borders">
+                        <div class="row">
+                            <div class="col-lg-3 scol-auto m-auto border-danger borders">
+                                <i class="fas fa-book-open" style="font-size: 50px;"></i>
+                            </div>
+                            <div class="col-9 px-0 m-auto borders border-primary">
+                                <h6 class="m-0">Education</h6>
+                                <p style="font-size: small;" class="text-muted m-0">This is a wider card with to additional <br> additional content.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- START 2nd-card -->
+                <div class="col-lg-4 sw-25 sborder bg-lights p-3s spy-4 mx-1s sshadow-sm rounded-lg">
+                    <div class="col-lg-12 border-primary shadow-sm rounded-lg py-4 bg-light borders">
+                        <div class="row">
+                            <div class="col-lg-3 scol-auto m-auto border-danger borders">
+                                <i class="fas fa-book-open" style="font-size: 50px;"></i>
+                            </div>
+                            <div class="col-9 px-0 m-auto borders border-primary">
+                                <h6 class="m-0">Education</h6>
+                                <p style="font-size: small;" class="text-muted m-0">This is a wider card with to additional <br> additional content.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- START 3rd-card -->
+                <div class="col-lg-4 mb-4 sw-25 sborder bg-lights p-3s spy-4 mx-1s sshadow-sm rounded-lg">
+                    <div class="col-lg-12 border-primary shadow-sm rounded-lg py-4 bg-light borders">
+                        <div class="row">
+                            <div class="col-lg-3 scol-auto m-auto border-danger borders">
+                                <i class="fas fa-book-open" style="font-size: 50px;"></i>
+                            </div>
+                            <div class="col-9 px-0 m-auto borders border-primary">
+                                <h6 class="m-0">Education</h6>
+                                <p style="font-size: small;" class="text-muted m-0">This is a wider card with to additional <br> additional content.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- START 4th-card -->
+                <div class="col-lg-4 sw-25 sborder bg-lights p-3s spy-4 mx-1s sshadow-sm rounded-lg">
+                    <div class="col-lg-12 border-primary shadow-sm rounded-lg py-4 bg-light borders">
+                        <div class="row">
+                            <div class="col-lg-3 scol-auto m-auto border-danger borders">
+                                <i class="fas fa-book-open" style="font-size: 50px;"></i>
+                            </div>
+                            <div class="col-9 px-0 m-auto borders border-primary">
+                                <h6 class="m-0">Education</h6>
+                                <p style="font-size: small;" class="text-muted m-0">This is a wider card with to additional <br> additional content.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- START 5th-card -->
+                <div class="col-lg-4 sw-25 sborder bg-lights p-3s spy-4 mx-1s sshadow-sm rounded-lg">
+                    <div class="col-lg-12 border-primary shadow-sm rounded-lg py-4 bg-light borders">
+                        <div class="row">
+                            <div class="col-lg-3 scol-auto m-auto border-danger borders">
+                                <i class="fas fa-book-open" style="font-size: 50px;"></i>
+                            </div>
+                            <div class="col-9 px-0 m-auto borders border-primary">
+                                <h6 class="m-0">Education</h6>
+                                <p style="font-size: small;" class="text-muted m-0">This is a wider card with to additional <br> additional content.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- START 6th-card -->
+                <div class="col-lg-4 sw-25 sborder bg-lights p-3s spy-4 mx-1s sshadow-sm rounded-lg">
+                    <div class="col-lg-12 border-primary shadow-sm rounded-lg py-4 bg-light borders">
+                        <div class="row">
+                            <div class="col-lg-3 scol-auto m-auto border-danger borders">
+                                <i class="fas fa-book-open" style="font-size: 50px;"></i>
+                            </div>
+                            <div class="col-9 px-0 m-auto borders border-primary">
+                                <h6 class="m-0">Education</h6>
+                                <p style="font-size: small;" class="text-muted m-0">This is a wider card with to additional <br> additional content.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- END 6th-card -->
+            </div>
+        </div>
+    </div>
+    <!-- END - Main-Container of what-we-doo -->
+
+
+<!-- End Main Page Section -->
+
+
+<?php get_footer();?>
