eb0e0e82242d030a1c6dd2b05533a5e39c9c77b4
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-07-02 02:45:24 +0500

    page.php created.

---
+++ actionmboka/wp-content/themes/NGO-2020/page.php
@@ -0,0 +1,16 @@
+<?php
+    /* 
+    Template Name: main-template 
+    */
+    get_header();
+?>
+
+    <?php 
+    
+        while (have_posts()): 
+            //the_post();
+            the_content();
+        endwhile;
+    ?>
+
+<? get_footer(); ?>
