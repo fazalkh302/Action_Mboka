439a624080172b442e064d49fd57dd7846ab102b
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-07-02 03:58:54 +0500

    Project Wordpress Updated.

--- actionmboka/wp-content/themes/NGO-2020/header.php
+++ actionmboka/wp-content/themes/NGO-2020/header.php
@@ -1,48 +1,55 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes();?>>
+
 <head>
     <!-- bootstrap css -->
-    <link href="<?php bloginfo('template_url'); ?>/assets/css/bootstrap.css" rel="stylesheet">
-    <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
-    
+    <link href="<?php bloginfo('template_url');?>/assets/css/bootstrap.css" rel="stylesheet">
+    <link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet">
+
     <!-- Awosome fonts icons -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/solid.css" integrity="sha384-VGP9aw4WtGH/uPAOseYxZ+Vz/vaTb1ehm1bwx92Fm8dTrE+3boLfF1SpAtB1z7HW" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/regular.css" integrity="sha384-ZlNfXjxAqKFWCwMwQFGhmMh3i89dWDnaFU2/VZg9CvsMGA7hXHQsPIqS+JIAmgEq" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/brands.css" integrity="sha384-rf1bqOAj3+pw6NqYrtaE1/4Se2NBwkIfeYbsFdtiR6TQz0acWiwJbv1IM/Nt/ite" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/fontawesome.css" integrity="sha384-1rquJLNOM3ijoueaaeS5m+McXPJCGdr5HcA03/VHXxcp2kX2sUrQDmFc3jR5i/C7" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/solid.css"
+        integrity="sha384-VGP9aw4WtGH/uPAOseYxZ+Vz/vaTb1ehm1bwx92Fm8dTrE+3boLfF1SpAtB1z7HW" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/regular.css"
+        integrity="sha384-ZlNfXjxAqKFWCwMwQFGhmMh3i89dWDnaFU2/VZg9CvsMGA7hXHQsPIqS+JIAmgEq" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/brands.css"
+        integrity="sha384-rf1bqOAj3+pw6NqYrtaE1/4Se2NBwkIfeYbsFdtiR6TQz0acWiwJbv1IM/Nt/ite" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/fontawesome.css"
+        integrity="sha384-1rquJLNOM3ijoueaaeS5m+McXPJCGdr5HcA03/VHXxcp2kX2sUrQDmFc3jR5i/C7" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta charset="<?php bloginfo('charset');?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php bloginfo('name'); ?> | <?php wp_title(); ?></title>
-    <?php wp_head(); ?>
+    <title><?php bloginfo('name');?> | <?php wp_title();?></title>
+    <?php wp_head();?>
 </head>
+
 <body>
-    
+
     <!-- top contact area -->
-    <div class="container-fluid borders border-dark py-1 text-white position-relative" style="box-shadow: 0px 5px 12px 3px #0000006b; background-color: #1d2a3b;">
+    <div class="container-fluid borders border-dark py-1 text-white position-relative"
+        style="box-shadow: 0px 5px 12px 3px #0000006b; background-color: #1d2a3b;">
         <div class="container borders border-warning">
             <!-- <div class="col-lg-12 borders border-danger"> -->
-                <div class="row align-items-centers">
-                    <div class="col-lg-2 borders border-primary">
-                        <h6 class="m-0 mt-1" style="font-size: small;">admin@admin.com</h6>
-                    </div>
-                    <div class="col-lg-3 borders border-primary">
-                        <h6 class="m-0 mt-1" style="font-size: small;">+9200000000</h6>
-                    </div>
-                    <div class="col-lg-3 ml-auto pr-0s border-primary borders">
-                        <div class="col-12">
-                            <div class="row">
-                                <i class="fab fa-facebook-f px-2 mt-2s" style="font-size: small; line-height: 2;"></i>
-                                <i class="fab fa-twitter px-2" style="font-size: small; line-height: 2;"></i>
-                                <i class="fab fa-instagram px-2 ml-autos" style="font-size: small; line-height: 2;"></i>
-                                <a href="#" class="ml-auto text-white">
-                                    <h6 class="h6 mt-1" style="font-size: small;">Login</h6>
-                                </a>
-                            </div>
+            <div class="row align-items-centers">
+                <div class="col-lg-2 borders border-primary">
+                    <h6 class="m-0 mt-1" style="font-size: small;">admin@admin.com</h6>
+                </div>
+                <div class="col-lg-3 borders border-primary">
+                    <h6 class="m-0 mt-1" style="font-size: small;">+9200000000</h6>
+                </div>
+                <div class="col-lg-3 ml-auto pr-0s border-primary borders">
+                    <div class="col-12">
+                        <div class="row">
+                            <i class="fab fa-facebook-f px-2 mt-2s" style="font-size: small; line-height: 2;"></i>
+                            <i class="fab fa-twitter px-2" style="font-size: small; line-height: 2;"></i>
+                            <i class="fab fa-instagram px-2 ml-autos" style="font-size: small; line-height: 2;"></i>
+                            <a href="#" class="ml-auto text-white">
+                                <h6 class="h6 mt-1" style="font-size: small;">Login</h6>
+                            </a>
                         </div>
                     </div>
                 </div>
+            </div>
             <!-- </div> -->
         </div>
     </div>
@@ -55,37 +62,52 @@
                 <a class="navbar-brand borders border-primary col-2" href="#">
                     <img src="/assets/images/Action_20MBOKA-04 (1).png" class="img-fluid" alt="">
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
                 </button>
-              
+
                 <div class="collapse navbar-collapse borders border-warning" id="navbarSupportedContent">
-                  <ul class="navbar-nav mr-auto m-auto borders border-primary">
-                    <li class="nav-item active">
-                      <a class="nav-link mx-2 text-white" style="font-weight: 600; border-bottom: 2px solid #4362af;" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link mx-2 text-white" style="font-weight: 600;" href="#">Mission</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link mx-2 text-white" style="font-weight: 600;" href="#">Objective</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link mx-2 text-white" style="font-weight: 600;" href="#">Make Donation</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link mx-2 text-white" style="font-weight: 600;" href="#">News</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link mx-2 text-white" style="font-weight: 600;" href="#">About Us</a>
-                    </li>
-                  </ul>
-                  <form class="form-inline">
-                    <button class="btn btn-warning py-2 px-4 rounded-sm" style="font-size: small; font-weight: 600;" type="submit">DONATE NOW</button>
-                  </form>
+                    <!-- <ul class="navbar-nav mr-auto m-auto borders border-primary">
+                        <li class="nav-item active">
+                            <a class="nav-link mx-2 text-white"
+                                style="font-weight: 600; border-bottom: 2px solid #4362af;" href="#">Home <span
+                                    class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link mx-2 text-white" style="font-weight: 600;" href="#">Mission</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link mx-2 text-white" style="font-weight: 600;" href="#">Objective</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link mx-2 text-white" style="font-weight: 600;" href="#">Make Donation</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link mx-2 text-white" style="font-weight: 600;" href="#">News</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link mx-2 text-white" style="font-weight: 600;" href="#">About Us</a>
+                        </li>
+                    </ul> -->
+                    <?php
+                        wp_nav_menu(
+                            array(
+                                'theme_location' => 'top',
+                                'container' => '',
+                                'menu_class' => 'navbar-nav main__menu',
+                                'menu' => 'Main Navigation Menu',
+                            )
+                        );
+                    ?>
+                    <form class="form-inline">
+                        <button class="btn btn-warning py-2 px-4 rounded-sm" style="font-size: small; font-weight: 600;"
+                            type="submit">DONATE NOW</button>
+                    </form>
                 </div>
 
-              </nav>
-              <!-- end navbar -->
+            </nav>
+            <!-- end navbar -->
         </div>
     </div>
--- actionmboka/wp-content/themes/NGO-2020/page.php
+++ actionmboka/wp-content/themes/NGO-2020/page.php
@@ -1,16 +1,21 @@
 <?php
-    /* 
-    Template Name: main-template 
-    */
-    get_header();
+/*
+Template Name: main-template
+ */
+get_header();
 ?>
 
-    <?php 
-    
-        while (have_posts()): 
-            //the_post();
-            the_content();
-        endwhile;
-    ?>
+<main class="page_main">
+    <section class="content py-5">
+        <div class="container">
+            <?php 
+                while (have_posts()): 
+                    the_post(); 
+                    the_content(); 
+                endwhile; // End of the loop. 
+            ?>
+        </div>
+    <section>
+</main>
 
-<? get_footer(); ?>
+<?php get_footer(); ?>
