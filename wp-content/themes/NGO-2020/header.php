32920caa143b63870d1c9c853f2df40fdb7faf68
fazalkh302 <62914421+fazalkh302@users.noreply.github.com>
2020-07-01 21:57:54 +0500

    data abase fille

--- wp-content/themes/NGO-2020/header.php
+++ wp-content/themes/NGO-2020/header.php
@@ -15,6 +15,7 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo('name'); ?> | <?php wp_title(); ?></title>
+    <?php include_once('function.php'); ?>
     <?php wp_head(); ?>
 </head>
 <body>
@@ -52,8 +53,8 @@
         <div class="container borders border-warning">
             <!-- start navbar -->
             <nav class="navbar navbar-expand-lg navbar-light borders border-dark px-0">
-                <a class="navbar-brand borders border-primary col-2" href="#">
-                    <img src="<?php bloginfo('template_url'); ?>/assets/images/Action_20MBOKA-04 (1).png" class="img-fluid" alt="">
+                <!-- <a class="navbar-brand borders border-primary col-2" href="#">
+                    <img src="/assets/images/Action_20MBOKA-04 (1).png" class="img-fluid" alt="">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                   <span class="navbar-toggler-icon"></span>
@@ -83,7 +84,59 @@
                   <form class="form-inline">
                     <button class="btn btn-warning py-2 px-4 rounded-sm" style="font-size: small; font-weight: 600;" type="submit">DONATE NOW</button>
                   </form>
-                </div>
+                </div> -->
+                <?php
+                
+                // wp_nav_menu( array $args = array(
+                //     'menu'              => "", // (int|string|WP_Term) Desired menu. Accepts a menu ID, slug, name, or object.
+                //     'menu_class'        => "navbar-nav mr-auto", // (string) CSS class to use for the ul element which forms the menu. Default 'menu'.
+                //     'menu_id'           => "", // (string) The ID that is applied to the ul element which forms the menu. Default is the menu slug, incremented.
+                //     'container'         => "div", // (string) Whether to wrap the ul, and what to wrap it with. Default 'div'.
+                //     'container_class'   => "collapse navbar-collapse", // (string) Class that is applied to the container. Default 'menu-{menu slug}-container'.
+                //     'container_id'      => "bs-example-navbar-collapse-1", // (string) The ID that is applied to the container.
+                //     'fallback_cb'       => "WP_Bootstrap_Navwalker::fallback", // (callable|bool) If the menu doesn't exists, a callback function will fire. Default is 'wp_page_menu'. Set to false for no fallback.
+                //     'before'            => "", // (string) Text before the link markup.
+                //     'after'             => "", // (string) Text after the link markup.
+                //     'link_before'       => "", // (string) Text before the link text.
+                //     'link_after'        => "", // (string) Text after the link text.
+                //     'echo'              => "", // (bool) Whether to echo the menu or return it. Default true.
+                //     'depth'             => 2, // (int) How many levels of the hierarchy are to be included. 0 means all. Default 0.
+                //     'walker'            => new WP_Bootstrap_Navwalker(), // (object) Instance of a custom walker class.
+                //     'theme_location'    => "primary", // (string) Theme location to be used. Must be registered with register_nav_menu() in order to be selectable by the user.
+                //     'items_wrap'        => "", // (string) How the list items should be wrapped. Default is a ul with an id and class. Uses printf() format with numbered placeholders.
+                //     'item_spacing'      => "", // (string) Whether to preserve whitespace within the menu's HTML. Accepts 'preserve' or 'discard'. Default 'preserve'.
+                // ) );
+                // wp_nav_menu( array $args = array(
+                //     'menu'              => "", // (int|string|WP_Term) Desired menu. Accepts a menu ID, slug, name, or object.
+                //     'menu_class'        => "", // (string) CSS class to use for the ul element which forms the menu. Default 'menu'.
+                //     'menu_id'           => "", // (string) The ID that is applied to the ul element which forms the menu. Default is the menu slug, incremented.
+                //     'container'         => "", // (string) Whether to wrap the ul, and what to wrap it with. Default 'div'.
+                //     'container_class'   => "", // (string) Class that is applied to the container. Default 'menu-{menu slug}-container'.
+                //     'container_id'      => "", // (string) The ID that is applied to the container.
+                //     'fallback_cb'       => "", // (callable|bool) If the menu doesn't exists, a callback function will fire. Default is 'wp_page_menu'. Set to false for no fallback.
+                //     'before'            => "", // (string) Text before the link markup.
+                //     'after'             => "", // (string) Text after the link markup.
+                //     'link_before'       => "", // (string) Text before the link text.
+                //     'link_after'        => "", // (string) Text after the link text.
+                //     'echo'              => "", // (bool) Whether to echo the menu or return it. Default true.
+                //     'depth'             => "", // (int) How many levels of the hierarchy are to be included. 0 means all. Default 0.
+                //     'walker'            => "", // (object) Instance of a custom walker class.
+                //     'theme_location'    => "", // (string) Theme location to be used. Must be registered with register_nav_menu() in order to be selectable by the user.
+                //     'items_wrap'        => "", // (string) How the list items should be wrapped. Default is a ul with an id and class. Uses printf() format with numbered placeholders.
+                //     'item_spacing'      => "", // (string) Whether to preserve whitespace within the menu's HTML. Accepts 'preserve' or 'discard'. Default 'preserve'.
+                // ) );
+
+                    wp_nav_menu( array(
+                        'theme_location'  => 'primary',
+                        'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
+                        'container'       => 'div',
+                        'container_class' => 'collapse navbar-collapse',
+                        'container_id'    => 'bs-example-navbar-collapse-1',
+                        'menu_class'      => 'navbar-nav mr-auto',
+                        'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+                        'walker'          => new WP_Bootstrap_Navwalker())
+                    );
+                ?>
               </nav>
               <!-- end navbar -->
         </div>
